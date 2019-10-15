23a36a6f419435f83e90ad789c22fd85efb65ac7
Teofilus Candra <teofiluschandra@gmail.com>
2019-10-15 16:38:43 +0800

    change folder name && fixed netlify build error

--- components/BlogSection/index.js
+++ components/BlogSection/index.js
@@ -1,13 +1,13 @@
 import Title from '../Title'
 import Separator from '../Separator'
-import BlogCard from '../BlogCard'
+import PostCard from '../PostCard'
 
 const BlogSection = (props) => (
     <>
         <Title title="Blog"/>
         <Separator />
         {props.posts.map((item, key) => {
-            return <BlogCard {...item} key={key} />
+            return <PostCard {...item} key={key} />
         })}
     </>
 )
