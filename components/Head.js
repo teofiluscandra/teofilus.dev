facdf06e93b4fa9d1bf50659245929e54b16a615
Teofilus Candra <teofiluschandra@gmail.com>
2019-11-14 10:11:22 +0800

    fix: Adding path to style css

--- components/Head.js
+++ components/Head.js
@@ -18,6 +18,8 @@ const Head = props => (
         <link rel="apple-touch-icon" href="/static/touch-icon.png" />
         <link rel="mask-icon" href="/static/favicon-mask.svg" color="#49B882" />
         <link rel="icon" href="/static/favicon.ico" />
+        <link rel="stylesheet" href="/static/style.css" />
+
         <meta property="og:url" content={props.url || defaultOGURL} />
         <meta property="og:title" content={props.title || ''} />
         <meta
--- components/Layout.js
+++ components/Layout.js
@@ -5,6 +5,7 @@ import "../style.css";
 
 const Layout = props => (
     <>
+        <Head />
         <Nav />
         <div className="w-full py-24 px-6 relative z-10 max-w-2xl mx-auto text-left">
             {props.children}
