de91cfb51660322838b7f099598b961b54d61cb3
Teofilus Candra <teofiluschandra@gmail.com>
2021-07-02 11:13:34 +0700

    PPKM Darurat

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,29 +1,4 @@
-import { Box, chakra, Container, Stack, Text, useColorModeValue, VisuallyHidden } from '@chakra-ui/react';
-import { ReactNode } from 'react';
-
-const SocialButton = ({ children, label, href }: { children: ReactNode; label: string; href: string }) => {
-  return (
-    <chakra.button
-      bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
-      rounded={'full'}
-      w={8}
-      h={8}
-      cursor={'pointer'}
-      as={'a'}
-      href={href}
-      display={'inline-flex'}
-      alignItems={'center'}
-      justifyContent={'center'}
-      transition={'background 0.3s ease'}
-      _hover={{
-        bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200'),
-      }}
-    >
-      <VisuallyHidden>{label}</VisuallyHidden>
-      {children}
-    </chakra.button>
-  );
-};
+import { Box, Container, Stack, Text, useColorModeValue } from '@chakra-ui/react';
 
 const Footer = (): JSX.Element => {
   return (
