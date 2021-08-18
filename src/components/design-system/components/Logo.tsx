bb4087526d0a6e95e1aa4acd2a91951e8e06da1d
Teofilus Candra <teofiluschandra@gmail.com>
2021-08-18 11:34:50 +0800

    chore: upgrade package dependencies

--- src/components/design-system/components/Logo.tsx
+++ src/components/design-system/components/Logo.tsx
@@ -1,10 +1,13 @@
 import { URL_PHOTO } from '@/lib/constants';
-import { Box, FlexProps, Image } from '@chakra-ui/react';
+import { Box, FlexProps, Img } from '@chakra-ui/react';
+import LazyLoad from 'react-lazyload';
 
 const Logo = (props?: FlexProps): JSX.Element => {
   return (
     <Box {...props}>
-      <Image src={URL_PHOTO} alt="Logo Web Teofilus.dev" />
+      <LazyLoad height={50}>
+        <Img src={URL_PHOTO} alt="Logo Web Teofilus.dev" />
+      </LazyLoad>
     </Box>
   );
 };
