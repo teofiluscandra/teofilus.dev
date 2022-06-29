236d819919e5768fb3d5bc80907f5dfe952a1fdf
Teofilus Candra <teofiluschandra@gmail.com>
2022-06-29 17:16:47 +0700

    style: add experiment projects

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -12,6 +12,7 @@ interface Project {
     alt: string;
     url: string;
   };
+  isexperiment: boolean;
 }
 
 interface ProjectPageProps {
@@ -24,6 +25,7 @@ const PROJECT_QUERY = ` query getAllProjects {
     title
     description
     link
+    isexperiment
     image {
       alt
       url
@@ -44,34 +46,48 @@ export async function getStaticProps() {
   };
 }
 
+const ProjectCard = ({ item }: { item: Project }) => (
+  <SimpleGrid key={item.id} as="li" columns={{ base: 1, md: 2 }} gap={{ base: '1', md: '4' }}>
+    <Box>
+      <Image src={item.image.url} alt={item.image.alt} borderRadius="10" objectFit="cover" />
+    </Box>
+    <Box>
+      <Heading as="h5" size="md" color="secondary" my="2">
+        {item.title}
+      </Heading>
+      <Text mb="2" fontSize="sm">
+        {item.description}
+      </Text>
+      <NextLink href={item.link} passHref>
+        <Link color="secondary" isExternal>
+          Visit website →
+        </Link>
+      </NextLink>
+    </Box>
+  </SimpleGrid>
+);
+
+const HeadingProject = ({ title }: { title: string }) => (
+  <Heading as="h1" size="lg" mb="10" mt="10">
+    {title}
+  </Heading>
+);
+
 const Projects = ({ data }: ProjectPageProps) => {
   return (
     <Layout title="My Projects">
       <Container maxW="3xl">
-        <Heading as="h1" size="lg" mb="10">
-          Selected Projects
-        </Heading>
+        <HeadingProject title="Selected Projects" />
         <Stack as="ul" spacing="2rem" divider={<StackDivider w="70%" borderColor="gray" />}>
-          {data.map((item: Project) => (
-            <SimpleGrid key={item.id} as="li" columns={{ base: 1, md: 2 }} gap={{ base: '1', md: '4' }}>
-              <Box>
-                <Image src={item.image.url} alt={item.image.alt} borderRadius="10" objectFit="cover" />
-              </Box>
-              <Box>
-                <Heading as="h5" size="md" color="secondary" my="2">
-                  {item.title}
-                </Heading>
-                <Text mb="2" fontSize="sm">
-                  {item.description}
-                </Text>
-                <NextLink href={item.link} passHref>
-                  <Link color="secondary" isExternal>
-                    Visit website →
-                  </Link>
-                </NextLink>
-              </Box>
-            </SimpleGrid>
-          ))}
+          {data.map((item: Project) => {
+            return !item.isexperiment ? <ProjectCard item={item} /> : null;
+          })}
+        </Stack>
+        <HeadingProject title="Experiment" />
+        <Stack as="ul" spacing="2rem" divider={<StackDivider w="70%" borderColor="gray" />}>
+          {data.map((item: Project) => {
+            return item.isexperiment ? <ProjectCard item={item} /> : null;
+          })}
         </Stack>
       </Container>
     </Layout>
